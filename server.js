1c6107e3cc511f299cdebac37fbc1a8c2d20fdad
moch akbar maulana <moc.akbar119@gmail.com>
2022-09-16 08:14:48 +0700

    initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({});
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.body.createdAt = Date.now();
+    }
+    next();
+});
+
+server.use(router);
+server.listen(port);
